94790b5c34bfeb5139fb2aaac8ddfc96a7fa2b39
JOY <jypjoy@gmail.com>
2023-05-02 09:29:09 +0900

    🎉 프로젝트 생성

---
+++ SagakSagak/SagakSagak/SagakSagakApp.swift
@@ -0,0 +1,17 @@
+//
+//  SagakSagakApp.swift
+//  SagakSagak
+//
+//  Created by Joy on 2023/05/02.
+//
+
+import SwiftUI
+
+@main
+struct SagakSagakApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
