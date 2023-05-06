aac06319da24585fa561ff26b2bc2e5f3dc71dca
JOY <jypjoy@gmail.com>
2023-05-07 07:34:09 +0900

    ✨ feat: 디자인 시스템 추가
    color blue b1, b2, b3 추가

---
+++ SagakSagak/SagakSagak/DesignSystem/Color/Colors.swift
@@ -0,0 +1,14 @@
+//
+//  Colors.swift
+//  SagakSagak
+//
+//  Created by Joy on 2023/05/07.
+//
+
+import SwiftUI
+
+extension Color {
+    static let b1 = Color(#colorLiteral(red: 0.8392156863, green: 0.9058823529, blue: 1, alpha: 1))
+    static let b2 = Color(#colorLiteral(red: 0.3411764706, green: 0.6156862745, blue: 1, alpha: 1))
+    static let b3 = Color(#colorLiteral(red: 0.9490196078, green: 0.968627451, blue: 1, alpha: 1))
+}
