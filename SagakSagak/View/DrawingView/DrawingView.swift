827efcf999350afd78b2c08c0abb6e8257b51801
MADElinessss <jyseen@naver.com>
2023-05-09 20:47:30 +0900

    ✨ feat: DrawingView 화면 crayon button 기능 수정
    따로따로 선택 됩니당

--- SagakSagak/View/DrawingView/DrawingView.swift
+++ SagakSagak/View/DrawingView/DrawingView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import ComposableArchitecture
 
 struct Line {
     var points : [CGPoint]
@@ -13,28 +14,40 @@ struct Line {
     var lineWidth : CGFloat
 }
 
-let crayonColors1: [(image: String, color: Color)] = [
-    ("redcrayon", .red),
-    ("orangecrayon", .orange),
-    ("yellowcrayon", .yellow),
-    ("greencrayon", .green),
-    ("bluecrayon", .blue),
-    ("purplecrayon", .purple)
+//let crayonColors1: [(image: String, color: Color)] = [
+//    ("redcrayon", Color.crayon1),
+//    ("orangecrayon", Color.crayon2),
+//    ("yellowcrayon", Color.crayon3),
+//    ("greencrayon", Color.crayon4),
+//    ("bluecrayon", Color.crayon5),
+//    ("purplecrayon", Color.crayon6)
+//]
+let crayonColors1: [(image: String, color: Color, id: Int)] = [
+    ("redcrayon", Color.red, 1),
+    ("orangecrayon", Color.orange, 2),
+    ("yellowcrayon", Color.yellow, 3),
+    ("greencrayon", Color.green, 4),
+    ("bluecrayon", Color.blue, 5),
+    ("purplecrayon", Color.purple, 6)
 ]
+
 let crayonColors2: [(image: String, color: Color)] = [
-    ("pinkcrayon", .pink),
-    ("browncrayon", .brown),
-    ("graycrayon", .gray),
-    ("blackcrayon", .black),
+    ("pinkcrayon", Color.crayon7),
+    ("browncrayon", Color.crayon8),
+    ("graycrayon", Color.crayon9),
+    ("blackcrayon", Color.crayon10),
 ]
 
 
 struct DrawingView: View {
     @State private var lines = [Line]()
     @State private var selectedColor : Color = .white
+    @State var selectedCrayon: Int = 0
     @State private var selectedWidth : CGFloat = 5
+    @State private var isDrawing : Bool = false
     @State private var isRedTapped : Bool = false
     @State private var isOrangeTapped : Bool = false
+    @EnvironmentObject private var coordinator: Coordinator
     
     var body: some View {
         ZStack {
@@ -46,38 +59,36 @@ struct DrawingView: View {
                 
                 HStack {
                     VStack{
-                        Spacer(minLength: 80)
-                        ForEach(crayonColors1, id: \.0) { crayon in
-                            Image(crayon.image)
-                            //.resizable()
-                                .scaledToFill()
-                                .frame(width: 108, height: 28)
-                                .padding(.bottom, 3)
-                                .padding(.trailing, 30)
-                                .onTapGesture {
-                                    selectedWidth = 5
-                                    selectedColor = crayon.color
-                                    isRedTapped = true
-                                }
-                                .offset(x: isRedTapped ? 20 : 0, y:-10)
+                        Spacer(minLength: 100)
+                        ForEach(crayonColors1, id: \.id) { crayon in
+                            Button(action: {
+                                selectedWidth = 5
+                                selectedColor = crayon.color
+                                selectedCrayon = crayon.id
+                            }, label: {
+                                Image(crayon.image)
+                                    .resizable()
+                                    .scaledToFill()
+                                    .frame(width: 108, height: 28)
+                                    .padding(.bottom, 3)
+                                    .padding(.trailing, 30)
+                                    .offset(x: selectedCrayon == crayon.id ? 20 : 0, y:-10)
+                                    .animation(.spring())
+                            })
                         }
                         Image("button_back")
                             .resizable()
                             .frame(width: 48, height: 48)
+//                            .padding(.top, 9)
                             .padding(.leading, 24)
                             .padding(.bottom, 24)
-                        
                     }
                     .padding(.trailing, 24)
-                  //  .layoutPriority(-30)
-                    
-                    
-                    
                     VStack {
                         Text("가람이에게 가장 소중한 것을 그려보자!")
+                            .font(FontManager.shared.nanumsquare(.extrabold, 18))
                             .frame(width: 300)
-                            .foregroundColor(.blue)
-                            .font(.system(size: 18))
+                            .foregroundColor(.system1)
                             .padding(.horizontal, 80)
                             .padding(.vertical, 15)
                             .background(Color.white)
@@ -95,6 +106,7 @@ struct DrawingView: View {
                             .background(Color.white)
                             .cornerRadius(50)
                             .gesture(DragGesture(minimumDistance: 0, coordinateSpace: .local).onChanged({ value in
+                                isDrawing = true
                                 let newPoint = value.location
                                 //newPoint : 그림 그리는 다음 위치
                                 /*
@@ -115,41 +127,44 @@ struct DrawingView: View {
                                 }
                             }))
                     }
-                    
-                   // .layoutPriority(100)
-                    
                     VStack() {
                         Image("button_exit")
                             .resizable()
                             .scaledToFit()
                             .frame(width: 48, height: 48)
-                            .padding(.bottom, 9)
-                            .padding(.trailing, 24)
+//                            .padding(.bottom, 9)
+                            .padding(.trailing, 48)
                             .padding(.top, 24)
                         ForEach(crayonColors2, id: \.0) { crayon in
-                            Image(crayon.image)
-                                .scaledToFill()
-                                .frame(width: 108, height: 28)
-                                .padding(.bottom, 3)
-                                .padding(.leading, 30)
-                                .onTapGesture {
-                                    selectedWidth = 5
-                                    selectedColor = crayon.color
-                                    isRedTapped = true
-                                }
-                                .offset(x: isRedTapped ? -20 : 1, y: 0)
+                            Button(action: {
+                                selectedWidth = 5
+                                selectedColor = crayon.color
+                                isRedTapped = true
+                            }, label: {
+                                Image(crayon.image)
+                                    .resizable()
+                                    .scaledToFill()
+                                    .frame(width: 108, height: 28)
+                                    .padding(.bottom, 3)
+                                    .padding(.leading, 30)
+                                    .offset(x: isRedTapped ? -20 : 1, y: 0)
+                            })
                         }
-                        Image("eraser")
-//                            .resizable()
-                            .offset(x: 21, y: 0)
-                            .frame(width: 108, height: 68)
-                            .onTapGesture {
-                                selectedWidth = 20
-                                selectedColor = .white
-                            }
-                            .padding(.bottom, 3)
-                            .padding(.trailing, 24)
-                        Image("button_next")
+                        Button(action: {
+                            selectedWidth = 20
+                            selectedColor = .white
+                            
+                        }) {
+                            Image("eraser")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 108, height: 68)
+                                .offset(x: selectedWidth == 20 ? -21 : 0, y: 0)
+                                .padding(.bottom, 3)
+                                .padding(.trailing, 24)
+                        }
+
+                        Image(isDrawing ? "button_next" : "button_next_enabled")
                             .padding(.bottom, 10)
                             .padding(.trailing, 24)
                     }
@@ -161,7 +176,7 @@ struct DrawingView: View {
             
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .background(Color(red: 226/255, green: 246/255, blue: 240/255, opacity: 1.0))
+        .background(Color.bg2)
         .ignoresSafeArea()
         
     }
