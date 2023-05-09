5496ed7d248b1cd850c41609aebffdf9c93d8ac4
JOY <jypjoy@gmail.com>
2023-05-09 11:29:00 +0900

    ✨ feat: 디자인시스템에 Const 구조체 추가

--- SagakSagak/SagakSagak/DesignSystem/Font/FontManager.swift
+++ SagakSagak/SagakSagak/DesignSystem/Font/FontManager.swift
@@ -23,7 +23,7 @@ struct FontManager {
     /// 나눔스퀘어 라운드 폰트를 type, size에 따라 return해 줍니다
     /// - Parameter Request: type , size
     /// - Returns: 나눔스퀘어라운드폰트의  type, size에 따른 커스텀 폰트 생성
-    func nanumsquare(_ type:  NanumSquareRound, _ size: CGFloat) -> Font { ////Info - Fonts provided by application에 꼭 폰트 등록해야 함
+    func nanumsquare(_ type:  NanumSquareRound, _ size: CGFloat) -> Font { 
         let name = "NanumSquareRound" + type.rawValue
         return Font.custom(name, size: size)
     }
